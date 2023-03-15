019ec0fd669331b1a661da1bdeb5838085555aec
Ekaterina Grebtsova <ekaterina.grebtsova@convexitydmcc.com>
2023-03-15 17:37:57 +0800

    lesson 22 regexp group examples

--- lesson-022-regex/src/main/java/NumbersValidation.java
+++ lesson-022-regex/src/main/java/NumbersValidation.java
@@ -2,17 +2,28 @@ import java.io.*;
 import java.util.regex.Pattern;
 
 public class NumbersValidation {
-    private static final Pattern pattern = Pattern.compile(".*");
+    // +7-123-456-78-90
+    // +7 123 456 78 90
+//    private static final Pattern pattern = Pattern.compile("\\+\\d{1,3}[- ]\\d{3}[- ]\\d{3}[- ]\\d{2}[- ]\\d{2}");
+    private static final Pattern pattern = Pattern.compile("re\\w{7}([- ])(\\w{1,3})(notehutneohu)");
 
     // игнорируем throws IOException
     public static void main(String[] args) throws IOException {
         var consoleReader = createConsoleReader();
+        System.out.println("Введите номер телефона:");
         String input = consoleReader.readLine();
 
         var matcher = pattern.matcher(input);
         if (matcher.matches()) {
-            System.out.println("pattern is matched");
+            System.out.println("Телефон корректный");
+            String matchedGroup = matcher.group(1);
+            String matchedGroup2 = matcher.group(2);
+            System.out.println("Совпавшая группа 1: " + matchedGroup);
+            System.out.println("Совпавшая группа 2: " + matchedGroup2);
+            System.out.println("Совпавшая группа 2: " + matcher.group(3));
             // some logic here
+        } else {
+            System.out.println("Телефон некорректный. Формат: +7-123-456-78-90");
         }
     }
 
